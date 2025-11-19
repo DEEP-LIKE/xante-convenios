74f894e518f2d9d7517810fdada97343d475c2d8
Juan Ruiz <jr311_@hotmail.com>
2025-11-18 20:59:25 -0600

    Ft: Add cronjob

--- routes/console.php
+++ routes/console.php
@@ -3,6 +3,15 @@
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
+use Illuminate\Support\Facades\Schedule;
+use App\Jobs\SyncHubspotClientsJob;
+
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+// Programación de Sincronización HubSpot (3 veces al día)
+Schedule::job(new SyncHubspotClientsJob())->dailyAt('09:00')->timezone('America/Mexico_City');
+Schedule::job(new SyncHubspotClientsJob())->dailyAt('13:00')->timezone('America/Mexico_City');
+Schedule::job(new SyncHubspotClientsJob())->dailyAt('16:00')->timezone('America/Mexico_City');
+
