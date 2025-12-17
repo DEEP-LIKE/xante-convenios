d7fd5fd2b151b5eb67a1f037e1f2f2f4012809c7
Juan Ruiz <jr311_@hotmail.com>
2025-12-16 21:08:49 -0600

    feat: Add DatabaseSeeder to orchestrate initial database population and display access credentials.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,7 +13,7 @@ class DatabaseSeeder extends Seeder
         
         $this->call([
             UserSeeder::class,
-            ClientSeeder::class,
+            // ClientSeeder::class,
             PropertySeeder::class,
             AgreementSeeder::class,
         ]);
