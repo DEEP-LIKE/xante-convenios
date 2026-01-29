4e9025870258de2b8066e54146da53a2cc81eb23
Juan Ruiz <jr311_@hotmail.com>
2026-01-29 00:00:28 -0600

    Fix: update

--- app/Filament/Schemas/ManageDocuments/StepTwoSchema.php
+++ app/Filament/Schemas/ManageDocuments/StepTwoSchema.php
@@ -10,10 +10,24 @@ use Filament\Schemas\Components\Grid;
 use Filament\Schemas\Components\Section;
 use Illuminate\Support\HtmlString;
 
+use App\Models\ClientDocument;
+use Illuminate\Support\Facades\Storage;
+
 class StepTwoSchema
 {
     public static function make(ManageDocuments $page): array
     {
+        $getUploadedFileMetadata = function ($file) {
+            $document = ClientDocument::where('file_path', $file)->first();
+
+            return [
+                'name' => $document?->document_name ?? basename($file),
+                'size' => $document?->file_size ?? 0,
+                'type' => null, // Dejar nulo para evitar que FilePond intente detectar el tipo
+                'url' => Storage::disk('s3')->url($file),
+            ];
+        };
+
         return [
             Section::make('Documentos Requeridos del Cliente')
                 ->description(new HtmlString(
@@ -54,7 +68,10 @@ class StepTwoSchema
                                         return 'ine_'.now()->format('Y-m-d_H-i-s').'.'.$extension;
                                     })
                                     ->visibility('private')
-                                    ->fetchFileInformation(false)
+                                    ->previewable(false)
+                                    ->openable()
+                                    ->downloadable()
+                                    ->getUploadedFileUsing($getUploadedFileMetadata)
                                     ->afterStateUpdated(function ($state) use ($page) {
                                         $page->handleDocumentStateChange('holder_ine', 'titular', $state);
                                     }),
@@ -72,7 +89,10 @@ class StepTwoSchema
                                         return 'curp_'.now()->format('Y-m-d_H-i-s').'.'.$extension;
                                     })
                                     ->visibility('private')
-                                    ->fetchFileInformation(false)
+                                    ->previewable(false)
+                                    ->openable()
+                                    ->downloadable()
+                                    ->getUploadedFileUsing($getUploadedFileMetadata)
                                     ->afterStateUpdated(function ($state) use ($page) {
                                         $page->handleDocumentStateChange('holder_curp', 'titular', $state);
                                     }),
@@ -90,7 +110,10 @@ class StepTwoSchema
                                         return 'constancia_fiscal_'.now()->format('Y-m-d_H-i-s').'.'.$extension;
                                     })
                                     ->visibility('private')
-                                    ->fetchFileInformation(false)
+                                    ->previewable(false)
+                                    ->openable()
+                                    ->downloadable()
+                                    ->getUploadedFileUsing($getUploadedFileMetadata)
                                     ->afterStateUpdated(function ($state) use ($page) {
                                         $page->handleDocumentStateChange('holder_fiscal_status', 'titular', $state);
                                     }),
@@ -108,7 +131,10 @@ class StepTwoSchema
                                         return 'comprobante_domicilio_vivienda_'.now()->format('Y-m-d_H-i-s').'.'.$extension;
                                     })
                                     ->visibility('private')
-                                    ->fetchFileInformation(false)
+                                    ->previewable(false)
+                                    ->openable()
+                                    ->downloadable()
+                                    ->getUploadedFileUsing($getUploadedFileMetadata)
                                     ->afterStateUpdated(function ($state) use ($page) {
                                         $page->handleDocumentStateChange('holder_proof_address_home', 'titular', $state);
                                     }),
@@ -126,7 +152,10 @@ class StepTwoSchema
                                         return 'comprobante_domicilio_titular_'.now()->format('Y-m-d_H-i-s').'.'.$extension;
                                     })
                                     ->visibility('private')
-                                    ->fetchFileInformation(false)
+                                    ->previewable(false)
+                                    ->openable()
+                                    ->downloadable()
+                                    ->getUploadedFileUsing($getUploadedFileMetadata)
                                     ->afterStateUpdated(function ($state) use ($page) {
                                         $page->handleDocumentStateChange('holder_proof_address_titular', 'titular', $state);
                                     }),
@@ -144,7 +173,10 @@ class StepTwoSchema
                                         return 'acta_nacimiento_'.now()->format('Y-m-d_H-i-s').'.'.$extension;
                                     })
                                     ->visibility('private')
-                                    ->fetchFileInformation(false)
+                                    ->previewable(false)
+                                    ->openable()
+                                    ->downloadable()
+                                    ->getUploadedFileUsing($getUploadedFileMetadata)
                                     ->afterStateUpdated(function ($state) use ($page) {
                                         $page->handleDocumentStateChange('holder_birth_certificate', 'titular', $state);
                                     }),
@@ -161,7 +193,10 @@ class StepTwoSchema
                                         return 'acta_matrimonio_'.now()->format('Y-m-d_H-i-s').'.'.$extension;
                                     })
                                     ->visibility('private')
-                                    ->fetchFileInformation(false)
+                                    ->previewable(false)
+                                    ->openable()
+                                    ->downloadable()
+                                    ->getUploadedFileUsing($getUploadedFileMetadata)
                                     ->afterStateUpdated(function ($state) use ($page) {
                                         $page->handleDocumentStateChange('holder_marriage_certificate', 'titular', $state);
                                     }),
@@ -179,7 +214,10 @@ class StepTwoSchema
                                         return 'estado_cuenta_'.now()->format('Y-m-d_H-i-s').'.'.$extension;
                                     })
                                     ->visibility('private')
-                                    ->fetchFileInformation(false)
+                                    ->previewable(false)
+                                    ->openable()
+                                    ->downloadable()
+                                    ->getUploadedFileUsing($getUploadedFileMetadata)
                                     ->afterStateUpdated(function ($state) use ($page) {
                                         $page->handleDocumentStateChange('holder_bank_statement', 'titular', $state);
                                     }),
@@ -205,7 +243,10 @@ class StepTwoSchema
                                         return 'instrumento_notarial_'.now()->format('Y-m-d_H-i-s').'.'.$extension;
                                     })
                                     ->visibility('private')
-                                    ->fetchFileInformation(false)
+                                    ->previewable(false)
+                                    ->openable()
+                                    ->downloadable()
+                                    ->getUploadedFileUsing($getUploadedFileMetadata)
                                     ->afterStateUpdated(function ($state) use ($page) {
                                         $page->handleDocumentStateChange('property_notarial_instrument', 'propiedad', $state);
                                     }),
@@ -223,7 +264,10 @@ class StepTwoSchema
                                         return 'recibo_predial_'.now()->format('Y-m-d_H-i-s').'.'.$extension;
                                     })
                                     ->visibility('private')
-                                    ->fetchFileInformation(false)
+                                    ->previewable(false)
+                                    ->openable()
+                                    ->downloadable()
+                                    ->getUploadedFileUsing($getUploadedFileMetadata)
                                     ->afterStateUpdated(function ($state) use ($page) {
                                         $page->handleDocumentStateChange('property_tax_receipt', 'propiedad', $state);
                                     }),
@@ -241,7 +285,10 @@ class StepTwoSchema
                                         return 'recibo_agua_'.now()->format('Y-m-d_H-i-s').'.'.$extension;
                                     })
                                     ->visibility('private')
-                                    ->fetchFileInformation(false)
+                                    ->previewable(false)
+                                    ->openable()
+                                    ->downloadable()
+                                    ->getUploadedFileUsing($getUploadedFileMetadata)
                                     ->afterStateUpdated(function ($state) use ($page) {
                                         $page->handleDocumentStateChange('property_water_receipt', 'propiedad', $state);
                                     }),
@@ -259,7 +306,10 @@ class StepTwoSchema
                                         return 'recibo_cfe_'.now()->format('Y-m-d_H-i-s').'.'.$extension;
                                     })
                                     ->visibility('private')
-                                    ->fetchFileInformation(false)
+                                    ->previewable(false)
+                                    ->openable()
+                                    ->downloadable()
+                                    ->getUploadedFileUsing($getUploadedFileMetadata)
                                     ->afterStateUpdated(function ($state) use ($page) {
                                         $page->handleDocumentStateChange('property_cfe_receipt', 'propiedad', $state);
                                     }),
