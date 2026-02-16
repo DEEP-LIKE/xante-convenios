9a093319d6e6fb273a4a17d25b73a1edc36e4fb4
Juan Ruiz <jr311_@hotmail.com>
2026-02-16 07:48:13 -0600

    feat: Create the `manage-documents.blade.php` Filament page, including a form and a conditional agreement recalculation modal.

--- resources/views/filament/pages/manage-documents.blade.php
+++ resources/views/filament/pages/manage-documents.blade.php
@@ -5,7 +5,7 @@
     
     <x-filament-actions::modals />
 
-    @if($this->record)
+    @if($this->agreement)
         <div 
             x-data="{ show: false }"
             x-on:open-recalculation-modal.window="show = true"
@@ -21,7 +21,7 @@
                 <span class="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
 
                 <div x-show="show" x-transition:enter="ease-out duration-300" x-transition:enter-start="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95" x-transition:enter-end="opacity-100 translate-y-0 sm:scale-100" x-transition:leave="ease-in duration-200" x-transition:leave-start="opacity-100 translate-y-0 sm:scale-100" x-transition:leave-end="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95" class="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-4xl sm:w-full">
-                    @livewire('agreement-recalculation-modal', ['agreementId' => $this->record->id])
+                    @livewire('agreement-recalculation-modal', ['agreementId' => $this->agreement->id])
                 </div>
             </div>
         </div>
